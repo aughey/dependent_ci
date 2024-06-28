ad4c9d35689db8e02b4bf9708e8d92cca1f6f3a7
John Aughey <jha@aughey.com>
2024-06-28 12:24:06 +0000

    sfdfdsa

--- src/app/hello_world.cpp
+++ src/app/hello_world.cpp
@@ -1,7 +1,11 @@
 #include "local/Hello.h"
+#include "library1/Hello.h"
+#include "library2/Hello.h"
 
 int main(int, char *[])
 {
     local::Hello::SayHello();
+    library1::Hello::SayHello();
+    library2::Hello::SayHello();
     return 0;
 }
